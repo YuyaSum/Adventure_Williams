dd5787bf6d3fcd96c1ced172e17165b82d3e7274
YuyaSum <hate4g@gmail.com>
2023-06-04 15:45:23 -0700

    Add project files.

---
+++ Adventure_Williams/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Adventure_Williams.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
